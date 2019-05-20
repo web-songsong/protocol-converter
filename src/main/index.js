252f1c2cedabc4485e7b511ba7ab92ca3f1a7887
charmingsong <web_songsong@163.com>
2019-05-20 18:37:00 +0800

    开发

--- src/main/index.js
+++ src/main/index.js
@@ -5,26 +5,30 @@ import { app, BrowserWindow } from 'electron'
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = require('path')
+    .join(__dirname, '/static')
+    .replace(/\\/g, '\\\\')
 }
 
 let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+const winURL =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`
 
-function createWindow () {
+function createWindow() {
   /**
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 330,
     useContentSize: true,
-    width: 1000
+    width: 598,
+    // frame: false,
+    backgroundColor: '#2d2d2d',
+    opacity: 0.85
   })
-
   mainWindow.loadURL(winURL)
-
   mainWindow.on('closed', () => {
     mainWindow = null
   })
--- src/renderer/components/home/index.vue
+++ src/renderer/components/home/index.vue
@@ -1,12 +1,75 @@
 <template>
-  <div class="home">
-    word
-    <button @click="$router.push('/home')">adf</button>
+  <div class="home"
+       @drag="handleClick">
+    <div class="content">
+      将文件拖放到这里
+    </div>
+    <div class="operation"
+         v-if="show">
+      <div class="btn_wrap">
+        <span class="btn">复制代码</span>
+        <span class="btn">预览</span>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log('handleClick')
+    },
+    song() {
+      console.log('xx')
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 .home {
-  color: red;
+  .content {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate3D(-50%, -50%, 0);
+    color: #fff;
+    width: 142px;
+    height: 142px;
+    border: 2px dashed #7e7d7c;
+    border-radius: 8px;
+    line-height: 142px;
+    font-size: 14px;
+    text-align: center;
+  }
+  .operation {
+    position: fixed;
+    bottom: 0px;
+    width: 100%;
+    height: 30px;
+    border: 1px solid #555555;
+    .btn_wrap {
+      display: flex;
+      position: absolute;
+      height: 16px;
+      right: 25px;
+      top: 6px;
+      align-items: center;
+      .btn {
+        background: #636363;
+        border-radius: 4px;
+        padding: 0 5px;
+        height: 16px;
+        font-size: 12px;
+        color: #ffffff;
+        margin-right: 10px;
+      }
+    }
+  }
 }
 </style>
